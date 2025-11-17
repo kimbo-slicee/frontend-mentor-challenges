5e5bfa06f28a4a8f3dca996b2bdc60e9a2ecfb78
mohammed el aouri <mohammedelaouri@gmail.com>
2025-11-17 23:35:13 +0100

    `Feat:add form validation using js and regex Patterns`

--- 06-Newsletter/script.js
+++ 06-Newsletter/script.js
@@ -1,2 +1,37 @@
 const emailInput=document.querySelector("input[type='email']");
-console.log(emailInput)
+const messageError=document.querySelector(".newsletter__error");
+const input=document.querySelector("input");
+const button=document.querySelector("button[type='submit']");
+
+const changeBorderColor=(valid,input)=>input.classList[valid?"remove":"add"]("inValid");
+
+const validation=(email)=>{
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    messageError.innerText=``;
+    if (!email){
+        messageError.innerText="this filed is require";
+        changeBorderColor(false,input);
+        return
+    } else if(!emailRegex.test(email)){
+        changeBorderColor(false,input);
+        messageError.innerText="Valid Email Require";
+        return;
+    }
+    changeBorderColor(true,input);
+    button.disabled=false;
+}
+const handelEmailValidation=(e)=>{
+    const email=e.target.value.trim();
+    button.disabled=true;
+    validation(email);
+}
+const handelFormSubmit=(e)=>{
+
+}
+
+
+/**/
+emailInput.addEventListener("input",handelEmailValidation);
+/**/
+button.addEventListener("click",handelFormSubmit)
+
